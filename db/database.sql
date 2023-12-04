bcf954d86fdd15c9fa8ccceb6a76f4b088e860d8
Armin <armin.lepuschitz@webwerk.at>
2023-12-04 10:03:56 +0100

    db plan

---
+++ db/database.sql
@@ -0,0 +1,120 @@
+#TODO:
+#   LOOK OVER TABLE ORDER
+#   LOOK OVER IT BRUH
+#   parent category
+#   
+
+CREATE TABLE Status {
+    ID int NOT NULL AUTO_INCREMENT,
+    status varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Washing_mode {
+    ID int NOT NULL AUTO_INCREMENT,
+    washing_mode varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Category{
+    ID int NOT NULL AUTO_INCREMENT,
+    type varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Brand{
+    ID int NOT NULL AUTO_INCREMENT,
+    name varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Material{
+    ID int NOT NULL AUTO_INCREMENT,
+    material varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE User{
+    ID int NOT NULL AUTO_INCREMENT,
+    name varchar(255),
+    email varchar(255),
+    password varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Weather{
+    ID int NOT NULL AUTO_INCREMENT,
+    weather varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Occasion{
+    ID int NOT NULL AUTO_INCREMENT,
+    occasion varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Outfit_type{
+    ID int NOT NULL AUTO_INCREMENT,
+    outfit_type varchar(255),
+    PRIMARY KEY (ID),
+};
+
+CREATE TABLE Outfit{
+    ID int NOT NULL AUTO_INCREMENT,
+    ID_user int,
+    ID_outfit_type smallint,
+    date DATE,
+    ID_weather int, 
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ID_user) REFERENCES User (ID),
+    FOREIGN KEY (ID_outfit_type) REFERENCES Outfit_type (ID),
+    FOREIGN KEY (ID_weather) REFERENCES Weather (ID),
+};
+
+CREATE TABLE Clothing {
+    ID int NOT NULL AUTO_INCREMENT,
+    name varchar(255),
+    ID_brand int,
+    ID_category int,
+    color varchar(255),
+    price float,
+    size varchar(255),
+    notes varchar(255),
+    ID_status int,
+    ID_washing_mode int,
+    ID_user int,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ID_brand) REFERENCES Brand (ID),
+    FOREIGN KEY (ID_category) REFERENCES Category (ID),
+    FOREIGN KEY (ID_status) REFERENCES Status (ID),
+    FOREIGN KEY (ID_washing_mode) REFERENCES Washing_mode (ID),
+    FOREIGN KEY (ID_user) REFERENCES User (ID),
+};
+
+CREATE TABLE Clothing_Material{
+    ID int NOT NULL AUTO_INCREMENT,
+    ID_clothing int,
+    ID_material int,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ID_clothing) REFERENCES Clothing (ID),
+    FOREIGN KEY (ID_material) REFERENCES Material (ID),
+};
+
+CREATE TABLE Outfit_Occasion{
+    ID int NOT NULL AUTO_INCREMENT,
+    ID_outfit int,
+    ID_occasion int,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ID_outfit) REFERENCES Outfit (ID),
+    FOREIGN KEY (ID_occasion) REFERENCES Occasion (ID),
+};
+
+CREATE TABLE Clothing_Outfit{
+    ID int NOT NULL AUTO_INCREMENT,
+    ID_clothing int,
+    ID_outfit int,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ID_clothing) REFERENCES Clothing (ID),
+    FOREIGN KEY (ID_outfit) REFERENCES Outfit (ID),
+};
