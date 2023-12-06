e17f14ac742b423bfeb7dbd9907197c2ac77114c
Armin <armin.lepuschitz@webwerk.at>
2023-12-06 12:35:16 +0100

    database planning

--- db/database.sql
+++ db/database.sql
@@ -1,78 +1,78 @@
 #TODO:
 #   LOOK OVER TABLE ORDER
 #   LOOK OVER IT BRUH
-#   parent category
+#  images
 #   
 
-CREATE TABLE Status {
+CREATE TABLE Status (
     ID int NOT NULL AUTO_INCREMENT,
     status varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Washing_mode {
+CREATE TABLE Washing_mode (
     ID int NOT NULL AUTO_INCREMENT,
     washing_mode varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Category{
+CREATE TABLE Category(
     ID int NOT NULL AUTO_INCREMENT,
     type varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Brand{
+CREATE TABLE Brand(
     ID int NOT NULL AUTO_INCREMENT,
     name varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Material{
+CREATE TABLE Material(
     ID int NOT NULL AUTO_INCREMENT,
     material varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE User{
+CREATE TABLE User(
     ID int NOT NULL AUTO_INCREMENT,
     name varchar(255),
     email varchar(255),
     password varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Weather{
+CREATE TABLE Weather(
     ID int NOT NULL AUTO_INCREMENT,
     weather varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Occasion{
+CREATE TABLE Occasion(
     ID int NOT NULL AUTO_INCREMENT,
     occasion varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Outfit_type{
+CREATE TABLE Outfit_type(
     ID int NOT NULL AUTO_INCREMENT,
     outfit_type varchar(255),
-    PRIMARY KEY (ID),
-};
+    PRIMARY KEY (ID)
+);
 
-CREATE TABLE Outfit{
+CREATE TABLE Outfit(
     ID int NOT NULL AUTO_INCREMENT,
     ID_user int,
-    ID_outfit_type smallint,
+    ID_outfit_type int,
     date DATE,
     ID_weather int, 
     PRIMARY KEY (ID),
     FOREIGN KEY (ID_user) REFERENCES User (ID),
     FOREIGN KEY (ID_outfit_type) REFERENCES Outfit_type (ID),
-    FOREIGN KEY (ID_weather) REFERENCES Weather (ID),
-};
+    FOREIGN KEY (ID_weather) REFERENCES Weather (ID)
+);
 
-CREATE TABLE Clothing {
+CREATE TABLE Clothing (
     ID int NOT NULL AUTO_INCREMENT,
     name varchar(255),
     ID_brand int,
@@ -89,32 +89,32 @@ CREATE TABLE Clothing {
     FOREIGN KEY (ID_category) REFERENCES Category (ID),
     FOREIGN KEY (ID_status) REFERENCES Status (ID),
     FOREIGN KEY (ID_washing_mode) REFERENCES Washing_mode (ID),
-    FOREIGN KEY (ID_user) REFERENCES User (ID),
-};
+    FOREIGN KEY (ID_user) REFERENCES User (ID)
+);
 
-CREATE TABLE Clothing_Material{
+CREATE TABLE Clothing_Material(
     ID int NOT NULL AUTO_INCREMENT,
     ID_clothing int,
     ID_material int,
     PRIMARY KEY (ID),
     FOREIGN KEY (ID_clothing) REFERENCES Clothing (ID),
-    FOREIGN KEY (ID_material) REFERENCES Material (ID),
-};
+    FOREIGN KEY (ID_material) REFERENCES Material (ID)
+);
 
-CREATE TABLE Outfit_Occasion{
+CREATE TABLE Outfit_Occasion(
     ID int NOT NULL AUTO_INCREMENT,
     ID_outfit int,
     ID_occasion int,
     PRIMARY KEY (ID),
     FOREIGN KEY (ID_outfit) REFERENCES Outfit (ID),
-    FOREIGN KEY (ID_occasion) REFERENCES Occasion (ID),
-};
+    FOREIGN KEY (ID_occasion) REFERENCES Occasion (ID)
+);
 
-CREATE TABLE Clothing_Outfit{
+CREATE TABLE Clothing_Outfit(
     ID int NOT NULL AUTO_INCREMENT,
     ID_clothing int,
     ID_outfit int,
     PRIMARY KEY (ID),
     FOREIGN KEY (ID_clothing) REFERENCES Clothing (ID),
-    FOREIGN KEY (ID_outfit) REFERENCES Outfit (ID),
-};
+    FOREIGN KEY (ID_outfit) REFERENCES Outfit (ID)
+);
