ae3491e4676f524826647d222a23f258833e81a5
Armin <armin.lepuschitz@webwerk.at>
2023-12-07 11:05:02 +0100

    maven dependencies etc

---
+++ app/src/main/java/com/project4app/App.java
@@ -0,0 +1,16 @@
+package com.project4app;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
