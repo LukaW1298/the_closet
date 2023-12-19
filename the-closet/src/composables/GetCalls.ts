d1a01ac947cac585f707f09f33b494c06ad2e6bf
crauscher-hit <crauscher@humanomed.at>
2023-12-19 01:32:46 +0100

    added composable for get calls

---
+++ the-closet/src/composables/GetCalls.ts
@@ -0,0 +1,21 @@
+import { ref } from 'vue';
+import { Category, Material } from '@/custom_types';
+
+const address = import.meta.env.VITE_API_ADDRESS;
+const requestOptions = {method: "GET", mode: "cors", redirect: "follow"};
+console.log("address", address)
+
+export async function getMaterials() {
+    return fetch(address + "/api/materials", {method: "GET", mode: "cors", redirect: "follow"})
+        .then(response => response.json())        
+}
+
+export async function getBrands() {
+    return fetch(address + "/api/brands", {method: "GET", mode: "cors", redirect: "follow"})
+        .then(response => response.json())        
+}
+
+export async function getWashingModes() {
+    return fetch(address + "/api/washingModes", {method: "GET", mode: "cors", redirect: "follow"})
+        .then(response => response.json())        
+}
