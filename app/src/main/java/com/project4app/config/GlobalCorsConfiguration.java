e75e86235092530ce6c3e82e75df948c5261d8ea
Armin <armin.lepuschitz@webwerk.at>
2024-01-10 11:17:39 +0100

    global cross annotation

---
+++ app/src/main/java/com/project4app/config/GlobalCorsConfiguration.java
@@ -0,0 +1,19 @@
+package com.project4app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class GlobalCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8100")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
