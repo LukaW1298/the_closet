c667b910cb6974b83164ec349c47ccfa5c4b937e
crauscher-hit <crauscher@humanomed.at>
2024-01-13 22:21:40 +0100

    fixed node modules and added eslint rules

--- the-closet/.eslintrc.cjs
+++ the-closet/.eslintrc.cjs
@@ -7,19 +7,98 @@ module.exports = {
   },
   plugins: [
     "vue",
-    '@typescript-eslint'
+    '@typescript-eslint',
+    '@stylistic',
+    "unicorn"
   ],
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'plugin:unicorn/recommended'
   ],
   parser: "vue-eslint-parser",
   parserOptions: {
     parser: '@typescript-eslint/parser',
   },
   rules: {
+    "@stylistic/semi": "warn",
+    "@stylistic/spaced-comment": ["warn", "always"],
+    "@stylistic/object-curly-newline": [
+      "error", {
+        "ObjectExpression": "always"
+      }
+    ],
+    "@stylistic/indent": ["warn", 2, {
+      "ObjectExpression": "first"
+    }],
+    '@stylistic/object-property-newline': "warn",
+    '@stylistic/lines-around-comment': ["warn", {
+      "beforeBlockComment": true,
+      "beforeLineComment": true
+    }],
+    "@stylistic/brace-style": ["warn", "stroustrup"],
+    "@stylistic/comma-spacing": ["warn", {
+      "before": false,
+      "after": true 
+    }],
+    "@stylistic/comma-style": ["warn", "last"],
+    "@stylistic/padding-line-between-statements": ["warn",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "return" 
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "block" 
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "block-like" 
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "const" 
+      },
+      {
+        blankLine: "any",
+        prev: "const",
+        next: "const" 
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "for" 
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "function" 
+      }
+    ],
+
+    // possible problems
+    'no-await-in-loop': "error",
+    "array-callback-return": "error",
+    "no-duplicate-imports": "error",
+    // eslint suggestions
+    "no-lonely-if": "error",
+    "sort-vars": "warn",
+    "sort-keys": "warn",
+    "arrow-body-style": ["warn", "always"],
+    "block-scoped-var": "error",
+    "camelcase": "warn",
+    "no-invalid-this": "warn",
+    "no-unused-expressions": "warn",
+    "no-useless-return": "warn",
+    "complexity": ["warn", 20],
+
+    // vue/ts specific rules
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     "vue/block-tag-newline": ["warn", {
@@ -39,7 +118,9 @@ module.exports = {
     "vue/no-useless-v-bind": "warn",
     "vue/no-useless-mustaches": "warn",
     "vue/padding-line-between-tags": ["warn", [{
-      blankLine: 'consistent', prev: '*', next: '*'
+      blankLine: 'consistent',
+      prev: '*',
+      next: '*'
     }]],
     "vue/prefer-define-options": "warn",
     "vue/prefer-separate-static-class": "warn",
@@ -48,7 +129,7 @@ module.exports = {
     "vue/max-attributes-per-line": ["warn", {
       "singleline": {
         "max": 2
-      },      
+      },
       "multiline": {
         "max": 2
       }
